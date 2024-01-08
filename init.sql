c96d77c56de040c728ace5fb8f8a319d436c0ae2
zaydanA <13521104@std.stei.itb.ac.id>
2024-01-08 18:15:03 +0700

    vehicle scheme

--- init.sql
+++ init.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    user_id      SERIAL PRIMARY KEY,
-    email        VARCHAR(256) UNIQUE NOT NULL,
-    username     VARCHAR(256) UNIQUE NOT NULL,
-    phone_number VARCHAR(256) UNIQUE NOT NULL,
-    password     VARCHAR(256) NOT NULL,
-    is_manager   BOOLEAN NOT NULL DEFAULT false
-);
+-- CREATE TABLE IF NOT EXISTS "user" (
+--     user_id      SERIAL PRIMARY KEY,
+--     email        VARCHAR(256) UNIQUE NOT NULL,
+--     username     VARCHAR(256) UNIQUE NOT NULL,
+--     password     VARCHAR(256) NOT NULL,
+--     is_manager   BOOLEAN NOT NULL DEFAULT false
+-- );
